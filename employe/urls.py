7aa619bdad1b0e0e3f480a5f6a2e4806c6895923
wadia20 <wadiasouiki@gmail.com>
2024-07-04 18:28:37 +0100

    first commit

---
+++ employe/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = "employe"
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_employe, name='login'),
+    
+]
---
+++ employe/views.py
@@ -0,0 +1,25 @@
+from .models import *
+from django.contrib import auth
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .form import *
+# home page
+def home(request):
+    return render(request, "home.html")
+
+#login page 
+def login_employe(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('pass')
+        user = authenticate(request, email=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.warning(request, 'something went wrong')
+            return redirect('users:login')
+    return render(request, 'login.html')
+
+
