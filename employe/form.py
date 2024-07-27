56b54dab847f931fdea0ba596a42441c580d89dc
ilyass-t <toumi.ilyass.04@gmail.com>
2024-07-27 13:30:54 +0100

    other staff left

--- employe/form.py
+++ employe/form.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Operation,Client
 
+
+
 class OperationForm(forms.ModelForm):
     class Meta:
         model = Operation
         fields = ['client_id', 'operation', 'observation', 'piece_jointe', 'confirmed']
+
+    
 
 class ClientForm(forms.ModelForm):
     class Meta:
